796c6ed6a7e06ca0ecedc29f4d6c8a427267338b
unknown <allgloryforrobots@gmail.com>
2021-04-11 21:48:00 +0300

    first commit

---
+++ keys.js
@@ -0,0 +1,3 @@
+module.exports = {
+	mongoURI: 'mongodb+srv://allgloryforrobots:02121991a@cluster0.wdfut.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
+}
---
+++ models/Position.js
@@ -0,0 +1,24 @@
+const mongoose = require('mongoose')
+const Scheme = mongoose.Schema
+
+const positionSchema = new Schema({
+	name: {
+		type: String,
+		required: true
+	},
+	cost: {
+		type: Number,
+		required: true
+	},
+	category: {
+		ref: 'categories',
+		type: Schema.Types.objectId
+	},
+	user: {
+		ref: 'users',
+		type: Schema.Types.objectId
+	}
+})
+
+
+module.exports = mongoose.model('positions', positionSchema)
