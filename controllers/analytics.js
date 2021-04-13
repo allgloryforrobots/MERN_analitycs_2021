b2c366e7ec88a473e5ac075dd568ab90fe696b67
unknown <allgloryforrobots@gmail.com>
2021-04-13 21:27:42 +0300

    'v5'

--- controllers/analytics.js
+++ controllers/analytics.js
@@ -3,5 +3,5 @@ module.exports.overview = function(req, res) {
 }
 
 module.exports.analytics = function(req, res) {
-	
+
 }
--- models/Position.js
+++ models/Position.js
@@ -1,24 +1,23 @@
 const mongoose = require('mongoose')
-const Scheme = mongoose.Schema
+const Schema = mongoose.Schema
 
 const positionSchema = new Schema({
-	name: {
-		type: String,
-		required: true
-	},
-	cost: {
-		type: Number,
-		required: true
-	},
-	category: {
-		ref: 'categories',
-		type: Schema.Types.objectId
-	},
-	user: {
-		ref: 'users',
-		type: Schema.Types.objectId
-	}
+  name: {
+    type: String,
+    required: true
+  },
+  cost: {
+    type: Number,
+    required: true
+  },
+  category: {
+    ref: 'categories',
+    type: Schema.Types.ObjectId
+  },
+  user: {
+    ref: 'users',
+    type: Schema.Types.ObjectId
+  }
 })
-
 
 module.exports = mongoose.model('positions', positionSchema)
